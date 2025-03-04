e3daaf4b32d1c5faa6786a31f014df5c1ec33b56
thanhphat113 <thanhphat9523@gmail.com>
2025-03-04 09:55:26 +0700

    dotnet8

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@ using ExcelToDB_Backend.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
 builder.Services.AddControllers();
 builder.Services.AddSwaggerGen();
 
@@ -27,11 +26,6 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseCors("AllowSpecificOrigins");
 
